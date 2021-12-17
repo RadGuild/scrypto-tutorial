ec9a68ac3fe52a540d3037de96882e94bb3b39d2
Rock Howard <rockmhoward@gmail.com>
2021-12-17 11:25:01 -0600

    updated for alexandria

--- token-creator/src/lib.rs
+++ token-creator/src/lib.rs
@@ -14,15 +14,18 @@ blueprint! {
                 .metadata("icon_url", "https://badge_website.com/icon.ico")
                 .metadata("url", "https://badge_website.com")
                 .initial_supply_fungible(1);
+
             // Create tokens with fixed supply of 1 000 000 000
-            let tokens: Bucket = ResourceBuilder::new_fungible(0)
+            let tokens = ResourceBuilder::new_fungible(DIVISIBILITY_MAXIMUM)
                 .metadata("name", "Really Cool Token")
                 .metadata("symbol", "RCT")
                 .initial_supply_fungible(1_000_000_000);
+
             let component = Self {
                 token_vault: Vault::with_bucket(tokens)
             }
             .instantiate();
+
             // Don't forget! At the end of a function of method,
             // all buckets must be stored in a vault or returned.
             (component, badge)
@@ -32,6 +35,7 @@ blueprint! {
             // Send up the badge or token address to learn more about it.
             // Of course you could just do "resim show $my_address" but
             // this shows how to get the metadata with Scrypto.
+
             let this_res_def = ResourceDef::from(addr);
             let metadata = this_res_def.metadata();
             if metadata.contains_key("name") && metadata.contains_key("symbol") {
@@ -43,7 +47,7 @@ blueprint! {
             if metadata.contains_key("icon_url") && metadata.contains_key("url") {
                 info!( "icon_url: {}, url: {}", metadata["icon_url"], metadata["url"]);
             }
-            info!( "supply: {}", this_res_def.supply());
+            info!( "supply: {}", this_res_def.total_supply());
         }
     }
 }
